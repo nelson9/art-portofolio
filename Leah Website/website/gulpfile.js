2d476e5fd907d6699612b6b0b922f4053af9b83b
nelson9 <niall.ferguson@gmail.com>
2016-08-01 00:08:55 +0100

    header

--- Leah Website/website/gulpfile.js
+++ Leah Website/website/gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/*.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/*.scss' , ['sass']);
 });
