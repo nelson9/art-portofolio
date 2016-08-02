6ee74234e7a7b8041886bbacbacb200b4ca3ffba
Niall Ferguson <nferguson@macmillan.org.uk>
2016-08-02 10:00:01 +0100

    update

--- Leah Website/website/gulpfile.js
+++ Leah Website/website/gulpfile.js
@@ -3,11 +3,11 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/*.scss')
+  return gulp.src('app/scss/styles.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('app/css'))
 });
 
 gulp.task('watch', function () {
-    gulp.watch('app/scss/*.scss' , ['sass']);
+    gulp.watch('app/scss/**/*.scss' , ['sass']);
 });
