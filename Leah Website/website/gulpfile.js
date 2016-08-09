e7151de18582587110b4b3d54941b68965440c0d
nelson9 <niall.ferguson@gmail.com>
2016-08-09 23:50:27 +0100

    add js compile to gulp

--- Leah Website/website/gulpfile.js
+++ Leah Website/website/gulpfile.js
@@ -1,13 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/styles.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('app/scss/**/*.scss' , ['sass']);
+});
+
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('app/js'));
 });
